1eaf625bde32a341a7fff21692d307dbaa345230
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-05-15 17:05:06 -0300

    update product

--- starter-vite/src/App.tsx
+++ starter-vite/src/App.tsx
@@ -2,12 +2,14 @@ import { Refine } from "@refinedev/core";
 
 import { dataProvider } from "./providers/data-provider";
 import { ShowProduct } from "./pages/products/show";
+import { EditProduct } from "./pages/products/edit";
 
 
 function App(): JSX.Element {
   return (
     <Refine dataProvider={dataProvider}>
-      <ShowProduct/>
+      {/* <ShowProduct/> */}
+      <EditProduct/>
     </Refine>
   );
 }
